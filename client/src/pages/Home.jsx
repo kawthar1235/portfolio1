db10447850ca1292448040be99d0d298967d6a70
Kawthar <kawthar1235@gmail.com>
2026-05-25 02:34:27 +0300

    fix: remove hero, add morph animation to about photo

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,4 @@
 import Loader from '../components/Loader';
-import Hero from '../components/Hero';
 import About from '../components/About';
 import Projects from '../components/Projects';
 import Skills from '../components/Skills';
@@ -9,7 +8,6 @@ export default function Home() {
   return (
     <>
       <Loader />
-      <Hero />
       <About />
       <Projects />
       <Skills />
