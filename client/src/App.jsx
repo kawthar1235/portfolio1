96ff65ea48c611da036e6de1a36d33b590b84271
Kawthar <kawthar1235@gmail.com>
2026-05-26 13:02:40 +0300

    g

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,9 @@
 import { Routes, Route, Outlet } from 'react-router-dom';
-
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-
 import Home from './pages/Home';
 import ProjectsPage from './pages/ProjectsPage';
 import NotFound from './pages/NotFound';
-
 import AdminDashboard from './components/AdminDashboard';
 
 function MainLayout() {
@@ -22,17 +19,12 @@ function MainLayout() {
 export default function App() {
   return (
     <Routes>
-
-      {/* Admin Page */}
       <Route path="/admin" element={<AdminDashboard />} />
-
-      {/* Main Website */}
       <Route element={<MainLayout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/projects" element={<ProjectsPage />} />
+        <Route index element={<Home />} />
+        <Route path="projects" element={<ProjectsPage />} />
         <Route path="*" element={<NotFound />} />
       </Route>
-
     </Routes>
   );
 }
