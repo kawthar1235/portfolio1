cbf8acf8337c300cd446cf3c5077c5c0f68f33a1
Kawthar <kawthar1235@gmail.com>
2026-05-26 12:31:04 +0300

    Fix routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,38 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Outlet } from 'react-router-dom';
+
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+
 import Home from './pages/Home';
 import ProjectsPage from './pages/ProjectsPage';
 import NotFound from './pages/NotFound';
+
 import AdminDashboard from './components/AdminDashboard';
+
+function MainLayout() {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  );
+}
 
 export default function App() {
   return (
-    <>
-      <Routes>
-        <Route path="/admin" element={<AdminDashboard />} />
-        <Route path="/*" element={
-          <>
-            <Navbar />
-            <Routes>
-              <Route path="/"         element={<Home />} />
-              <Route path="/projects" element={<ProjectsPage />} />
-              <Route path="*"         element={<NotFound />} />
-            </Routes>
-            <Footer />
-          </>
-        } />
-      </Routes>
-    </>
+    <Routes>
+
+      {/* Admin Page */}
+      <Route path="/admin" element={<AdminDashboard />} />
+
+      {/* Main Website */}
+      <Route element={<MainLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<ProjectsPage />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+
+    </Routes>
   );
 }
