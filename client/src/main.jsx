11f68f3ca18f26dcf8192b91553155521ffcce17
kawtharAli <s202168810@kfupm.edu.sa>
2026-05-30 16:55:19 +0300

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,14 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from './context/ThemeContext';
+import { LanguageProvider } from './context/LanguageContext';
 import App from './App';
 import './styles/global.css';
+import './styles/rtl.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <ThemeProvider>
-        <App />
+        <LanguageProvider>
+          <App />
+        </LanguageProvider>
       </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
